6871bc8574d38ee16255d1b77932e2e3c04add72
DamiaoRapha <damiaoraphael@hotmail.com>
2024-01-19 14:13:26 -0300

    Endereços - atalho

---
+++ script_endereços.js
@@ -0,0 +1,349 @@
+
+        document.getElementById('copyTextAgudos').addEventListener('click', function() {
+            copyToClipboard('R. Aparecida do Norte, 90 - Vila Honorina. CEP: 17120-000 https://goo.gl/maps/XpYpQcteLaLxLVoL9');
+        });
+
+        function copyToClipboard(text) {
+            var textArea = document.createElement('textarea');
+            textArea.value = text;
+
+            // Adiciona o elemento ao corpo do documento
+            document.body.appendChild(textArea);
+
+            // Seleciona o conteúdo do textarea
+            textArea.select();
+
+            // Executa o comando de cópia
+            document.execCommand('copy');
+
+            // Remove o textarea do documento
+            document.body.removeChild(textArea);
+
+            // Pode exibir uma mensagem ou fazer outras ações após a cópia
+            alert('ENDEREÇO COPIADO!');
+        }
+
+
+            document.getElementById('copyTextAraraquara').addEventListener('click', function() {
+                copyToClipboard('Av. Dr. Waldomiro Blundi, 54. CEP: 14807-422 https://goo.gl/maps/BoYbmgBNwgDmtyeK6');
+            });
+
+            function copyToClipboard(text) {
+                var textArea = document.createElement('textarea');
+                textArea.value = text;
+
+                // Adiciona o elemento ao corpo do documento
+                document.body.appendChild(textArea);
+
+                // Seleciona o conteúdo do textarea
+                textArea.select();
+
+                // Executa o comando de cópia
+                document.execCommand('copy');
+
+                // Remove o textarea do documento
+                document.body.removeChild(textArea);
+
+                // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                alert('ENDEREÇO COPIADO!');
+            }
+        
+
+                document.getElementById('copyTextAvare').addEventListener('click', function() {
+                    copyToClipboard('Sp 255 Km 264 Zona Rural , S/N - De frente com a INRODA https://maps.app.goo.gl/dhHBZNQUZw1tACz36');
+                });
+
+                function copyToClipboard(text) {
+                    var textArea = document.createElement('textarea');
+                    textArea.value = text;
+
+                    // Adiciona o elemento ao corpo do documento
+                    document.body.appendChild(textArea);
+
+                    // Seleciona o conteúdo do textarea
+                    textArea.select();
+
+                    // Executa o comando de cópia
+                    document.execCommand('copy');
+
+                    // Remove o textarea do documento
+                    document.body.removeChild(textArea);
+
+                    // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                    alert('ENDEREÇO COPIADO!');
+                }
+
+
+                    document.getElementById('copyTextBarretos').addEventListener('click', function() {
+                        copyToClipboard('Rua Altair Do Nascimento, 850. CEP: 14783-152 https://goo.gl/maps/eJSRbVytyUsfjARXA');
+                    });
+
+                    function copyToClipboard(text) {
+                        var textArea = document.createElement('textarea');
+                        textArea.value = text;
+
+                        // Adiciona o elemento ao corpo do documento
+                        document.body.appendChild(textArea);
+
+                        // Seleciona o conteúdo do textarea
+                        textArea.select();
+
+                        // Executa o comando de cópia
+                        document.execCommand('copy');
+
+                        // Remove o textarea do documento
+                        document.body.removeChild(textArea);
+
+                        // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                        alert('ENDEREÇO COPIADO!');
+                    }
+
+
+                        document.getElementById('copyTextBotucatu').addEventListener('click', function() {
+                            copyToClipboard('Avenida Das Hortências, 1703. CEP: 18065-258 https://goo.gl/maps/8DXA3Buy2xxngmTk6');
+                        });
+
+                        function copyToClipboard(text) {
+                            var textArea = document.createElement('textarea');
+                            textArea.value = text;
+
+                            // Adiciona o elemento ao corpo do documento
+                            document.body.appendChild(textArea);
+
+                            // Seleciona o conteúdo do textarea
+                            textArea.select();
+
+                            // Executa o comando de cópia
+                            document.execCommand('copy');
+
+                            // Remove o textarea do documento
+                            document.body.removeChild(textArea);
+
+                            // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                            alert('ENDEREÇO COPIADO!');
+                        }
+
+
+                            document.getElementById('copyTextBotucatu').addEventListener('click', function() {
+                                copyToClipboard('Avenida Das Hortências, 1703. CEP: 18065-258 https://goo.gl/maps/8DXA3Buy2xxngmTk6');
+                            });
+
+                            function copyToClipboard(text) {
+                                var textArea = document.createElement('textarea');
+                                textArea.value = text;
+
+                                // Adiciona o elemento ao corpo do documento
+                                document.body.appendChild(textArea);
+
+                                // Seleciona o conteúdo do textarea
+                                textArea.select();
+
+                                // Executa o comando de cópia
+                                document.execCommand('copy');
+
+                                // Remove o textarea do documento
+                                document.body.removeChild(textArea);
+
+                                // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                                alert('ENDEREÇO COPIADO!');
+                            }
+
+                            
+                                document.getElementById('copyTextCaraguatatuba').addEventListener('click', function() {
+                                    copyToClipboard('Rua Felix Jonas Amorim, 22. Próximo à policia rodoviária Tamoios https://maps.app.goo.gl/apRDtboUpdPSZdTz6');
+                                });
+
+                                function copyToClipboard(text) {
+                                    var textArea = document.createElement('textarea');
+                                    textArea.value = text;
+
+                                    // Adiciona o elemento ao corpo do documento
+                                    document.body.appendChild(textArea);
+
+                                    // Seleciona o conteúdo do textarea
+                                    textArea.select();
+
+                                    // Executa o comando de cópia
+                                    document.execCommand('copy');
+
+                                    // Remove o textarea do documento
+                                    document.body.removeChild(textArea);
+
+                                    // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                                    alert('ENDEREÇO COPIADO!');
+                                }
+
+
+                                    document.getElementById('copyTextCerqueira').addEventListener('click', function() {
+                                        copyToClipboard('Avenida João Cardoso de Oliveira, 870 - Centro. https://goo.gl/maps/hoRjHjehcePnUsuJA');
+                                    });
+        
+                                    function copyToClipboard(text) {
+                                        var textArea = document.createElement('textarea');
+                                        textArea.value = text;
+        
+                                        // Adiciona o elemento ao corpo do documento
+                                        document.body.appendChild(textArea);
+        
+                                        // Seleciona o conteúdo do textarea
+                                        textArea.select();
+        
+                                        // Executa o comando de cópia
+                                        document.execCommand('copy');
+        
+                                        // Remove o textarea do documento
+                                        document.body.removeChild(textArea);
+        
+                                        // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                                        alert('ENDEREÇO COPIADO!');
+                                    }
+
+
+                                        document.getElementById('copyTextGuararapes').addEventListener('click', function() {
+                                            copyToClipboard('Estrada Vicinal Natal Scatolin, S/N. Km 04 - Corrego Nascente. https://goo.gl/maps/uCSchwDjTZ5rFw3a9');
+                                        });
+            
+                                        function copyToClipboard(text) {
+                                            var textArea = document.createElement('textarea');
+                                            textArea.value = text;
+            
+                                            // Adiciona o elemento ao corpo do documento
+                                            document.body.appendChild(textArea);
+            
+                                            // Seleciona o conteúdo do textarea
+                                            textArea.select();
+            
+                                            // Executa o comando de cópia
+                                            document.execCommand('copy');
+            
+                                            // Remove o textarea do documento
+                                            document.body.removeChild(textArea);
+            
+                                            // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                                            alert('ENDEREÇO COPIADO!');
+                                        }
+
+
+                                            document.getElementById('copyTextJau').addEventListener('click', function() {
+                                                copyToClipboard('Av. Lourenço Neto De Almeida Prado, 1031 - Atrás do Território do Calçado. https://goo.gl/maps/sQncvsrvqdue4z6o6');
+                                            });
+                
+                                            function copyToClipboard(text) {
+                                                var textArea = document.createElement('textarea');
+                                                textArea.value = text;
+                
+                                                // Adiciona o elemento ao corpo do documento
+                                                document.body.appendChild(textArea);
+                
+                                                // Seleciona o conteúdo do textarea
+                                                textArea.select();
+                
+                                                // Executa o comando de cópia
+                                                document.execCommand('copy');
+                
+                                                // Remove o textarea do documento
+                                                document.body.removeChild(textArea);
+                
+                                                // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                                                alert('ENDEREÇO COPIADO!');
+                                            }
+
+
+                                                document.getElementById('copyTextMarilia').addEventListener('click', function() {
+                                                    copyToClipboard('Rodovia Comandante João Ribeiro De Barros, Sp 294 - Km 464, S/N. https://maps.app.goo.gl/aZAuKhEth87MSuQt5');
+                                                });
+                    
+                                                function copyToClipboard(text) {
+                                                    var textArea = document.createElement('textarea');
+                                                    textArea.value = text;
+                    
+                                                    // Adiciona o elemento ao corpo do documento
+                                                    document.body.appendChild(textArea);
+                    
+                                                    // Seleciona o conteúdo do textarea
+                                                    textArea.select();
+                    
+                                                    // Executa o comando de cópia
+                                                    document.execCommand('copy');
+                    
+                                                    // Remove o textarea do documento
+                                                    document.body.removeChild(textArea);
+                    
+                                                    // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                                                    alert('ENDEREÇO COPIADO!');
+                                                }
+
+
+                                                    document.getElementById('copyTextMirassol').addEventListener('click', function() {
+                                                        copyToClipboard('Av. Airton José Bilachi, 3965 - Próximo ao laboratório de alimentos Intertek Brasil. https://goo.gl/maps/8ChdzQtKAxVAKsHm7');
+                                                    });
+                        
+                                                    function copyToClipboard(text) {
+                                                        var textArea = document.createElement('textarea');
+                                                        textArea.value = text;
+                        
+                                                        // Adiciona o elemento ao corpo do documento
+                                                        document.body.appendChild(textArea);
+                        
+                                                        // Seleciona o conteúdo do textarea
+                                                        textArea.select();
+                        
+                                                        // Executa o comando de cópia
+                                                        document.execCommand('copy');
+                        
+                                                        // Remove o textarea do documento
+                                                        document.body.removeChild(textArea);
+                        
+                                                        // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                                                        alert('ENDEREÇO COPIADO!');
+                                                    }
+
+
+                                                        document.getElementById('copyTextMirassol').addEventListener('click', function() {
+                                                            copyToClipboard('Av. Airton José Bilachi, 3965 - Próximo ao laboratório de alimentos Intertek Brasil. https://goo.gl/maps/8ChdzQtKAxVAKsHm7');
+                                                        });
+                            
+                                                        function copyToClipboard(text) {
+                                                            var textArea = document.createElement('textarea');
+                                                            textArea.value = text;
+                            
+                                                            // Adiciona o elemento ao corpo do documento
+                                                            document.body.appendChild(textArea);
+                            
+                                                            // Seleciona o conteúdo do textarea
+                                                            textArea.select();
+                            
+                                                            // Executa o comando de cópia
+                                                            document.execCommand('copy');
+                            
+                                                            // Remove o textarea do documento
+                                                            document.body.removeChild(textArea);
+                            
+                                                            // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                                                            alert('ENDEREÇO COPIADO!');
+                                                        }
+
+
+                                                            document.getElementById('copyTextOurinhos').addEventListener('click', function() {
+                                                                copyToClipboard('Rua Mario de Oliveira Branco, 150 - Atras da Ouribram. https://goo.gl/maps/dzt6AecAoQnq99PW9');
+                                                            });
+                                
+                                                            function copyToClipboard(text) {
+                                                                var textArea = document.createElement('textarea');
+                                                                textArea.value = text;
+                                
+                                                                // Adiciona o elemento ao corpo do documento
+                                                                document.body.appendChild(textArea);
+                                
+                                                                // Seleciona o conteúdo do textarea
+                                                                textArea.select();
+                                
+                                                                // Executa o comando de cópia
+                                                                document.execCommand('copy');
+                                
+                                                                // Remove o textarea do documento
+                                                                document.body.removeChild(textArea);
+                                
+                                                                // Pode exibir uma mensagem ou fazer outras ações após a cópia
+                                                                alert('ENDEREÇO COPIADO!');
+                                                            }
